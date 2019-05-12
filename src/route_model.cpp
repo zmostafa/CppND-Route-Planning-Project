322f1bbb32289b5834089555ecb6589e4997d0c5
Ziad Mostafa <mohammedziad.badr@gmail.com>
2019-05-12 14:29:29 +0200

    implement router model lessons

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,5 +2,25 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
+    int n_Counter = 0;
+    for (auto node : this->Nodes()){
+        m_Nodes.push_back(Node(n_Counter, this, node));
+        n_Counter++;
+    }
+    // Create hashtable for node to road
+    CreateNodeToRoadHashmap();
+}
 
+void RouteModel::CreateNodeToRoadHashmap(){
+    auto n_Roads = Roads();
+    for (auto road : n_Roads){
+        if (road.type != Model::Road::Type::Footway){
+            for (auto node_idx : Ways()[road.way].nodes){
+                if ( node_to_road.find(node_idx) == node_to_road.end()){
+                    node_to_road[node_idx] = std::vector<const Model::Road *> {};
+                }
+                node_to_road[node_idx].push_back(&road);
+            }
+        }
+    }     
 }
--- src/route_model.h
+++ src/route_model.h
@@ -12,9 +12,15 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
-        
+        Node *parent = nullptr;
+        float h_value = std::numeric_limits<float>::max(); // intitalize value to max using stdlib
+        float g_value = 0.0 ;
+        bool visited = false ;
+        std::vector<Node *> neighbors {};
+
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
+        float distance (Node node) const { return sqrt( std::pow((x - node.x) ,2) + std::pow((y - node.y),2) ); } //  Return the euclidean distance from the current node to the node passed in.
       
       private:
         // Add private Node variables and methods here.
@@ -25,8 +31,14 @@ class RouteModel : public Model {
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    auto &SNodes () const { return m_Nodes;}
+    auto &GetNodeToRoadMap () const { return node_to_road;}
 
   private:
     // Add private RouteModel variables and methods here.
+    std::vector<Node> m_Nodes {};
+    std::unordered_map<int , std::vector<const Model::Road *> > node_to_road {} ;
+    void CreateNodeToRoadHashmap(); // his method will operate only on the `node_to_road` hashtable
+
 
 };
